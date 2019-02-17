607fa0493bce21982814593ec81b19bb304d615a
martin <kayondomartin45@gmail.com>
2019-02-17 22:59:24 +0900

    Initial Commit

---
+++ ProxAuth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProxAuth
+//
+//  Created by Martin on 17/02/2019.
+//  Copyright © 2019 Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
